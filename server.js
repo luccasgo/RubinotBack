741432cf73bb14893f0edf1315da6f08a1b3d13f
unknown <luccas-maciel@hotmail.com>
2024-07-17 12:19:03 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+const GUILD_URL = 'https://rubinot.com.br/?subtopic=guilds&page=view&GuildName=Ultimate%20Squad';
+
+// Adicionar middleware CORS
+app.use(cors());
+
+app.get('/api/guild', async (req, res) => {
+  try {
+    const response = await axios.get(GUILD_URL);
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const characters = [];
+    $('table.TableContent tbody tr').each((index, element) => {
+      const columns = $(element).find('td');
+      if (columns.length === 6) {
+        const rank = $(columns[0]).text().trim();
+        const nameAndTitle = $(columns[1]).find('a').text().trim();
+        const vocation = $(columns[2]).text().trim();
+        const level = $(columns[3]).text().trim();
+        const joiningDate = $(columns[4]).text().trim();
+        const status = $(columns[5]).find('.green').text().trim();
+
+        if (status.toLowerCase() === 'online') {
+          characters.push({
+            rank,
+            name: nameAndTitle,
+            vocation,
+            level,
+            joiningDate,
+            status
+          });
+        }
+      }
+    });
+
+    res.json(characters);
+  } catch (error) {
+    console.error('Error fetching guild data:', error);
+    res.status(500).send('Error fetching guild data');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
